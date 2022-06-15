b0e35bff4d69013c961bb13d5fbf49657562faa4
Milad Behrooz <99659323+miladbehrooz@users.noreply.github.com>
2022-06-15 11:31:14 +0200

    Update credentials.py

--- slack_bot/credentials.py
+++ slack_bot/credentials.py
@@ -1,2 +1,2 @@
-webhook_url = 'https://hooks.slack.com/services/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'
-webhook_url2 = 'https://hooks.slack.com/services/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'
+webhook_url = ''
+webhook_url2 = ''
